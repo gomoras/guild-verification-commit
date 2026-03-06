31f79057c9979bcbc4ba4defc1edd0ca2b11d38d
gomoras <90099973+gomoras@users.noreply.github.com>
2026-03-06 22:26:12 +0100

    Fix memory leak in event handlers

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-06T21:26:07.670Z
-Commit: Resolve timezone issues in date handling
+Timestamp: 2026-03-06T21:26:12.451Z
+Commit: Fix memory leak in event handlers
