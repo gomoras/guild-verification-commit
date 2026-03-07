0e2458704c7e6f057e565d3a404eb12fb463db76
gomoras <90099973+gomoras@users.noreply.github.com>
2026-03-07 20:40:29 +0100

    Fix broken links in documentation

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-07T19:40:26.423Z
-Commit: Patch API endpoint error handling
+Timestamp: 2026-03-07T19:40:29.342Z
+Commit: Fix broken links in documentation
