56633118df9f84a98198d8c08d8f2a83dd1834a5
gomoras <90099973+gomoras@users.noreply.github.com>
2026-03-11 14:05:44 +0100

    Add export functionality for reports

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-07T19:40:29.342Z
-Commit: Fix broken links in documentation
+Timestamp: 2026-03-11T13:05:43.696Z
+Commit: Add export functionality for reports
