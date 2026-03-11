f44ea618db52c6a14f74686bcb0d3ba492d29191
gomoras <90099973+gomoras@users.noreply.github.com>
2026-03-11 14:50:56 +0100

    Resolve timezone issues in date handling

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-07T19:40:20.851Z
-Commit: Correct typo in configuration file
+Timestamp: 2026-03-11T13:50:56.244Z
+Commit: Resolve timezone issues in date handling
