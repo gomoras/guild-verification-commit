cd73bb2dcdef42b90b8c1fc7b8e7696732d5062c
gomoras <90099973+gomoras@users.noreply.github.com>
2026-03-13 21:37:05 +0100

    Resolve timezone issues in date handling

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-11T14:48:20.093Z
-Commit: Introduce real-time notifications
+Timestamp: 2026-03-13T20:37:05.428Z
+Commit: Resolve timezone issues in date handling
